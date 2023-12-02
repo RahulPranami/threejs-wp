64c640aea02400d1ffa65223d6b0776481e69ecf
Rahul Pranami <rahulpranami101@gmail.com>
2023-12-02 20:40:19 +0530

    Initial Commit :tada:

---
+++ includes/class-threejs-wp-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://rahulpranami.co
+ * @since      1.0.0
+ *
+ * @package    Threejs_Wp
+ * @subpackage Threejs_Wp/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Threejs_Wp
+ * @subpackage Threejs_Wp/includes
+ * @author     Rahul Pranami <rahulpranami101@gmail.com>
+ */
+class Threejs_Wp_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-threejs-wp-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://rahulpranami.co
+ * @since      1.0.0
+ *
+ * @package    Threejs_Wp
+ * @subpackage Threejs_Wp/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Threejs_Wp
+ * @subpackage Threejs_Wp/includes
+ * @author     Rahul Pranami <rahulpranami101@gmail.com>
+ */
+class Threejs_Wp_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
